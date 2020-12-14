5247fbe46860a543b9bfc874839dd3eaf919b341
Sam Hollyer <rsamhollyer@gmail.com>
2020-12-14 10:10:32 -0600

    updated some fncs

--- models.js
+++ models.js
@@ -1,41 +1,42 @@
-const db = require("./product-db copy") //This is the products database
-const employees = require("./employees-db copy") //This is my employees lookuptable
+const db = require("./product-db copy"); //This is the products database
+const employees = require("./employees-db copy"); //This is my employees lookuptable
 
-const lookupTable = db.reduce((obj, item) => { //Want to convert my array into a lookuptable
-    obj[item.id] = {
-        ...item
-    };
-    return obj
-}, {})
-
-
+const lookupTable = db.reduce((obj, item) => {
+	//Want to convert my array into a lookuptable
+	obj[item.id] = {
+		...item,
+	};
+	return obj;
+}, {});
 
 const displayProduct = () => {
-    return db.map(p => `<div class="container"><a href="/products/${p.id}">${p.name}</a></div>`).join("")
-}
+	return db
+		.map(
+			(p) =>
+				`<div class="container"><a href="/products/${p.id}">${p.name}</a></div>`
+		)
+		.join("");
+};
 
 const singleProductDisplay = (someId) => {
-    return lookupTable[someId]
-}
-
-
+	return lookupTable[someId];
+};
 
 const employeeListing = () => {
-    return Object.keys(employees).map(k => {
-        let employee = employees[k]
-        employee.id = k
-        return employee
-    })
-}
+	return Object.keys(employees).map((k) => {
+		let employee = employees[k];
+		employee.id = k;
+		return employee;
+	});
+};
 
-const singleEmployee = (anID)=>{
-    return employees[anID]
-}
+const singleEmployee = (anID) => {
+	return employees[anID];
+};
 
-console.log(singleEmployee("8a33b43c-5827-49c5-bee7-5ad49a3ef7b4"));
 module.exports = {
-    displayProduct,
-    singleProductDisplay,
-    employeeListing,
-    singleEmployee,
-}
+	displayProduct,
+	singleProductDisplay,
+	employeeListing,
+	singleEmployee,
+};
