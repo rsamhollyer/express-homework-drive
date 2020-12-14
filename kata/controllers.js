726bb15a9ec18b5a2babe48053414f669ef34876
Sam Hollyer <rsamhollyer@gmail.com>
2020-12-14 08:50:29 -0600

    updating kata

--- kata/controllers.js
+++ kata/controllers.js
@@ -1,20 +1,49 @@
+const db = [
+	{
+		item: "space gun",
+		cost: "10",
+		id: 1,
+	},
+
+	{
+		item: "chew gum",
+		cost: "2",
+		id: 2,
+	},
+
+	{
+		item: "stuffy stuff",
+		cost: "12",
+		id: 3,
+	},
+];
+
 const layouts = {
-    partials: {
-        head:"/partials/headers",
-        foot:"/partials/footers"
-    }
-}
+	partials: {
+		head: "/partials/headers",
+		foot: "/partials/footers",
+	},
+};
 
 const home = (req, res) => {
-    res.render("index", {
-        ...layouts,
-        locals:{
-            greeting:"Hello World"
-        }
-    })
-}
+	res.render("index", {
+		...layouts,
+		locals: {
+			greeting: "Hello World",
+		},
+	});
+};
 
+const items = (req, res) => {
+	res.render("items", {
+		...layouts,
+		locals: {
+			items: db,
+		},
+	});
+};
 
-module.exports= {
-    home,
-}
+module.exports = {
+	home,
+	items,
+};
--- kata/index.js
+++ kata/index.js
@@ -1,42 +1,25 @@
-const http = require("http")
-const express = require("express")
-const app = express()
-const es6Renderer = require("express-es6-template-engine")
-const server = http.createServer(app)
-const PORT = 3000
-const HOST = "localhost"
-const controllers = require("./controllers")
+const http = require("http");
+const express = require("express");
+const app = express();
+const es6Renderer = require("express-es6-template-engine");
+const server = http.createServer(app);
+const PORT = 3000;
+const HOST = "localhost";
+const controllers = require("./controllers");
 
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
 
-const db = [
-    {
-        item: "space gun",
-        cost:"10"
-    },
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "html");
 
-    {
-        item:"chew gum",
-        cost:"2"
-    },
-
-    {
-        item:"stuffy stuff",
-        cost:"12"
-    }
-]
-
-app.use(express.urlencoded({
-    extended: true
-}))
-
-app.engine('html','es6Renderer')
-app.set("views","templates")
-app.set("view engine", "html")
-
-
-app.get("/",controllers.home)
-
+app.get("/", controllers.home);
+app.get("/items", controllers.items);
 
 server.listen(PORT, HOST, () => {
-    console.log(`http://${HOST}:${PORT}`);
-})
+	console.log(`http://${HOST}:${PORT}`);
+});
